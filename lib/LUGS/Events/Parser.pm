dbb271b43f3a3956ccff2fa6882bb0a8bc9a401e
Steven Schubiger <schubiger@cpan.org>
2012-05-30 22:00:59 +0200

    released 0.09

--- lib/LUGS/Events/Parser.pm
+++ lib/LUGS/Events/Parser.pm
@@ -11,7 +11,7 @@ use List::MoreUtils qw(all);
 use LUGS::Events::Parser::Event ();
 use Params::Validate ':all';
 
-our $VERSION = '0.08_02';
+our $VERSION = '0.09';
 
 validation_options(
     on_fail => sub
